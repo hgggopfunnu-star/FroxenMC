3aed44cf95a7d34ba25e2550344ef76b4b88a960
FroxenMC <hgggopfunnu@gmail.com>
2026-03-19 19:53:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits } = require("discord.js");
+const { Client, GatewayIntentBits, ActivityType } = require("discord.js");
 
 const client = new Client({
     intents: [
@@ -8,6 +8,20 @@ const client = new Client({
 
 client.once("ready", () => {
     console.log(`✅ FroxenMC Bot Online: ${client.user.tag}`);
+
+    client.user.setPresence({
+        status: "online", // online / idle / dnd / invisible
+        activities: [
+            {
+                name: "game.blockhost.cloud",
+                type: ActivityType.Watching
+            }
+        ]
+    });
 });
+
+// error protection (good for Railway)
+process.on("unhandledRejection", console.error);
+process.on("uncaughtException", console.error);
 
 client.login(process.env.TOKEN);
